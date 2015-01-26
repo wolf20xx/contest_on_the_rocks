998f93ed6b8f6cdf83b3836158d21d32e4cd1869
ikeomasaya <ikeomasaya@gmail.com>
2015-01-27 00:59:47 +0900

    Add Flying Action of aircraft

---
+++ Assets/move_aircraft.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var t = Time.deltaTime;
+var accel = 1000.0;
+var rot_power = 4.0;
+	
+function Start () {
+
+}
+
+function Update () {
+	rigidbody.AddForce(
+		(-transform.right * t * 2 + transform.forward * t) * accel ,
+		ForceMode.Impulse
+		) ;
+
+}
